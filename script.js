1f7b27ed6e089bf8269d645e8262b3b00c5b92b0
Varun Malaviya <varunmalaviya7@gmail.com>
2022-06-30 19:58:23 -0400

    Added more JS, restart button works now and resets the board. FIxed hover issue. Variable that switch sides did not chamge becaue it wasn't globally scoped. Added game logic to make endgame and draw functions.

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const placingAreas = document.querySelectorAll('.placingArea')
-const crossImage = 'https://library.kissclipart.com/20180902/xlq/kissclipart-orange-x-transparent-background-clipart-fossil-fue-a676aa326e9ea7a9.png'
-const circleImage = 'https://i.pinimg.com/originals/32/90/be/3290bed6840554f71997d071206fea64.png'
+const restartGame = document.getElementById('restartButton')
+const winningMessageElement = document.querySelector('.win-msg')
+const winningTextElement = document.querySelector('[data-winning-text]')
+const winning_results = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+let playerTurn
+// if playerTurn is true, its cross's turn otherwise its circle's turn.
 
-// if playerTurn is true, its cross's turn outwise its circle's turn.
-let playerTurn = true
+startGame()
 
-placingAreas.forEach(area => {
-    area.addEventListener('click', handleClick, {once: true})
-})
+restartGame.addEventListener('click', startGame)
+
+function startGame(){
+    playerTurn = false
+    placingAreas.forEach(area => {
+        area.classList.remove('cross')
+        area.classList.remove('circle')
+        area.removeEventListener('click', handleClick)
+        area.addEventListener('click', handleClick, {once: true})
+    })
+    switchSymbol()
+    winningMessageElement.classList.remove('show')
+}
+
 
 //handles what to do when a player clicks on the playing area
 //currently options are: places users symbol, swaps turn after every click
@@ -15,8 +38,14 @@ function handleClick(e) {
     const area = e.target
     const currentSymbol = playerTurn ? 'cross' : 'circle'
     placeSymbol(area, currentSymbol)
-    switchTurns()
-    switchSymbol()
+    if(checkWin(currentSymbol)){
+        endGame(false)
+    } else if (isDraw()) {
+        endGame(true)
+    } else {
+        switchTurns()
+        switchSymbol()
+    }
 }
 
 function placeSymbol(area,currentSymbol){
@@ -30,10 +59,35 @@ function switchTurns() {
 
 //currently not changing image on hover. I think its because i'm not assigning style properties to the right hover affect.
 //need to more work to find a solution.
+//should swithc to adding classes to playingArea and seeing if 3 of them are in a row!
 function switchSymbol() {
     if(playerTurn){
         document.documentElement.style.setProperty('--current-symbol', 'url(https://library.kissclipart.com/20180902/xlq/kissclipart-orange-x-transparent-background-clipart-fossil-fue-a676aa326e9ea7a9.png)');
     } else {
         document.documentElement.style.setProperty('--current-symbol', 'url(https://i.pinimg.com/originals/32/90/be/3290bed6840554f71997d071206fea64.png)');
     }
+}
+
+//function to check if a user won the game
+function checkWin(currentSymbol){
+    return winning_results.some(result => {
+        return result.every(index => {
+            return placingAreas[index].classList.contains(currentSymbol)
+        })
+    })
+}
+
+function endGame(draw){
+    if(draw){
+        winningTextElement.innerText = 'Draw!'
+    } else {
+        winningTextElement.innerText = `${playerTurn ? "X's" : "O's"} Wins!`
+    }
+    winningMessageElement.classList.add('show')
+}
+
+function isDraw(){
+    return [...placingAreas].every(area => {
+        return area.classList.contains('cross') || area.classList.contains('circle')
+    })
 }
